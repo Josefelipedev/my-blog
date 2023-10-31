60617e10b832923443d7ff2fdfa6a021328cf8ac
Jose Felipe Almeida dos Santos <josefelipedev@gmail.com>
2023-10-31 09:45:24 -0300

    feat: header profile

---
+++ src/components/Header/mock.ts
@@ -0,0 +1,9 @@
+import { HeaderProps } from '.';
+
+export default {
+  blogName: 'José Felipe ',
+  blogDescription:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.',
+  logo: '/assets/images/logo.jpeg',
+  showText: true,
+} as HeaderProps;
